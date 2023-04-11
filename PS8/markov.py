b4ff483b986ac9fb828f2a89b892276097a5288d
rendergame <admin@render.game>
2023-04-11 16:33:06 -0400

    start ps8

---
+++ PS8/markov.py
@@ -0,0 +1,19 @@
+from matrix_power import transition_matrix, IntMatrix
+
+initial_state: IntMatrix = transition_matrix
+
+'''
+
+Generate the transition matrix for every t between 0 and 100,000
+
+Starting at t = 1, iterate to 100,000
+    For every time t
+    Determine where to transition to next
+    Log the next transition destination
+    Log the transition edge taken
+    Follow the transition
+
+Determine where to transition to next
+
+
+'''
